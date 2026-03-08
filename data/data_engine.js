f67b5b36860f3f4340f66d02ed0e555f374bf6dc
BirVal <birmanbarrera@gmail.com>
2026-03-07 23:53:44 -0500

    Update data_engine.js

--- data/data_engine.js
+++ data/data_engine.js
@@ -1,1 +1,15 @@
+import {startStream} from "./websocket_stream.js"
+import {loadDataset} from "./dataset_loader.js"
+import {startRegime} from "../models/regime_model.js"
 
+export function startDataEngine(){
+
+const streamPanel = document.getElementById("streamPanel")
+const datasetPanel = document.getElementById("datasetPanel")
+const regimePanel = document.getElementById("regimePanel")
+
+startStream(streamPanel)
+loadDataset(datasetPanel)
+startRegime(regimePanel)
+
+}

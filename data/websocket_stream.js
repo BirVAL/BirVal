156c6f691b53ad44faf2263bc13bf73d632cca99
BirVal <birmanbarrera@gmail.com>
2026-03-07 23:50:51 -0500

    Update websocket_stream.js

--- data/websocket_stream.js
+++ data/websocket_stream.js
@@ -1,1 +1,44 @@
+export function startStream(panel){
 
+function connect(){
+
+try{
+
+const socket = new WebSocket(
+"wss://stream.binance.com:9443/ws/btcusdt@trade"
+)
+
+socket.onopen = function(){
+panel.innerHTML="Stream connected"
+}
+
+socket.onmessage = function(event){
+
+const data = JSON.parse(event.data)
+
+panel.innerHTML =
+"BTC Price: "+data.p+
+"<br>Trade Size: "+data.q
+
+}
+
+socket.onerror = function(){
+panel.innerHTML="Stream error"
+}
+
+socket.onclose = function(){
+panel.innerHTML="Reconnecting..."
+setTimeout(connect,3000)
+}
+
+}catch(e){
+
+panel.innerHTML="WebSocket failed"
+
+}
+
+}
+
+connect()
+
+}

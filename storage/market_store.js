a3308190f0fda2f65b509f34365e7137c61d4b67
BirVal <birmanbarrera@gmail.com>
2026-03-08 00:43:35 -0500

    Update market_store.js

--- storage/market_store.js
+++ storage/market_store.js
@@ -1,1 +1,34 @@
+let trades = []
+let candles = []
 
+export function storeTrade(trade){
+
+trades.push(trade)
+
+if(trades.length > 500){
+trades.shift()
+}
+
+}
+
+export function storeCandle(candle){
+
+candles.push(candle)
+
+if(candles.length > 500){
+candles.shift()
+}
+
+}
+
+export function getTrades(){
+
+return trades
+
+}
+
+export function getCandles(){
+
+return candles
+
+}

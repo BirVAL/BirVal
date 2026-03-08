faa9e32e96456be04a706cba069fa0b1e160cf80
BirVal <birmanbarrera@gmail.com>
2026-03-08 00:54:50 -0500

    Update data_engine.js

--- data/data_engine.js
+++ data/data_engine.js
@@ -2,14 +2,42 @@ import {startStream} from "./websocket_stream.js"
 import {loadDataset} from "./dataset_loader.js"
 import {startRegime} from "../models/regime_model.js"
 
+import {storeTrade, storeCandle} from "../storage/market_store.js"
+
+
 export function startDataEngine(){
 
 const streamPanel = document.getElementById("streamPanel")
 const datasetPanel = document.getElementById("datasetPanel")
 const regimePanel = document.getElementById("regimePanel")
 
-startStream(streamPanel)
-loadDataset(datasetPanel)
+
+/* =============================
+   MARKET STREAM
+============================= */
+
+startStream(streamPanel, (trade)=>{
+
+storeTrade(trade)
+
+})
+
+
+/* =============================
+   DATASET ENGINE
+============================= */
+
+loadDataset(datasetPanel, (candle)=>{
+
+storeCandle(candle)
+
+})
+
+
+/* =============================
+   MARKET REGIME MODEL
+============================= */
+
 startRegime(regimePanel)
 
 }

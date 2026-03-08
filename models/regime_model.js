33fa316db0a24701f394e64a91fd2f4213868e7f
BirVal <birmanbarrera@gmail.com>
2026-03-07 23:56:19 -0500

    Update regime_model.js

--- models/regime_model.js
+++ models/regime_model.js
@@ -1,1 +1,36 @@
+export function startRegime(panel){
 
+async function load(){
+
+try{
+
+const res = await fetch(
+"https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT"
+)
+
+const btc = await res.json()
+
+let change = parseFloat(btc.priceChangePercent)
+
+let regime = "SIDEWAYS"
+
+if(change > 2) regime = "BULL TREND"
+if(change < -2) regime = "BEAR TREND"
+
+panel.innerHTML =
+"BTC 24h: " + change + "%" +
+"<br>" + regime
+
+}catch(e){
+
+panel.innerHTML = "Regime error"
+
+}
+
+}
+
+load()
+
+setInterval(load,5000)
+
+}
